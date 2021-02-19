78816822a96cd899eab42a1dff869b32610ed7b2
emmanouela88 <76942908+emmanouela88@users.noreply.github.com>
2021-02-19 17:46:30 +0200

    few changes

---
+++ Assets/ARCamera.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//This script manipulates a 3D plane to project the camera as AR and as you rotate you can see a 3D obj.
+public class ARCamera : MonoBehaviour
+{
+    public GameObject obj;
+
+    void Start()
+    {
+        if (Application.isMobilePlatform)
+        {
+            GameObject cameraParent = new GameObject("camParent");
+            cameraParent.transform.position = this.transform.position;
+            this.transform.parent = cameraParent.transform;
+            cameraParent.transform.Rotate(Vector3.right, 90);
+        }
+
+        Input.gyro.enabled = true;
+        WebCamTexture webTexture = new WebCamTexture();
+        obj.GetComponent<MeshRenderer>().material.mainTexture = webTexture;
+        webTexture.Play();
+    }
+
+    void Update()
+    {
+        Quaternion camRotation = new Quaternion(Input.gyro.attitude.x, Input.gyro.attitude.y,
+            -Input.gyro.attitude.z, -Input.gyro.attitude.w);
+        this.transform.localRotation = camRotation;
+    }
+}
---
+++ Assets/GPSLocation.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GPSLocation : MonoBehaviour
+{
+    public Text GPSStatus;
+    public Text latitudePos;
+    public Text longitudePos;
+    public Text altitudeValue;
+    public Text accuracyVal;
+    public Text timeStampMine;
+
+    public static GPSLocation instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+        StartCoroutine(GPSLoc());
+    }
+
+    // Update is called once per frame
+    IEnumerator GPSLoc()
+    {
+        if (!Input.location.isEnabledByUser) yield break;
+
+        Input.location.Start();
+
+        int maxWait = 20;
+        while(Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+
+        if (maxWait < 1)
+        {
+            GPSStatus.text = "Time out";
+            yield break;
+        }
+
+        if(Input.location.status == LocationServiceStatus.Failed)
+        {
+            GPSStatus.text = "Unable for device location";
+            yield break;
+        }
+        else
+        {
+            GPSStatus.text = "Access Granted";
+            InvokeRepeating("UpdateGPSData",0.5f,1f);
+        }
+    }
+
+    private void UpdateGPSData()
+    {
+        if(Input.location.status == LocationServiceStatus.Running)
+        {
+            GPSStatus.text = "Access Granted";
+            latitudePos.text = Input.location.lastData.latitude.ToString();
+            longitudePos.text = Input.location.lastData.longitude.ToString();
+            altitudeValue.text = Input.location.lastData.altitude.ToString();
+            accuracyVal.text = Input.location.lastData.horizontalAccuracy.ToString();
+            timeStampMine.text = Input.location.lastData.timestamp.ToString();
+        }
+        else
+        {
+            GPSStatus.text = "No puedo";
+        }
+    }
+}
