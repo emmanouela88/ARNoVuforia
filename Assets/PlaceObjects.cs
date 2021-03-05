d00cff285bf6ffaf834596497636c8928a21bbf2
emmanouela88 <76942908+emmanouela88@users.noreply.github.com>
2021-03-05 16:53:35 +0200

    SmallUpdates

---
+++ Assets/PlaceObjects.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class PlaceObjects : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject placedObject;
+
+    [SerializeField]
+    private GameObject parentObject;
+    /* [SerializeField]
+     private Camera arCamera;*/
+
+    /*private Vector2 touchPosition = default;
+
+    private static List<ARRaycastHit> hits = new List<ARRaycastHit>();*/
+
+    private void Awake()
+    {
+        ChangeSelectedObject();
+    }
+
+    void ChangeSelectedObject()
+    {
+      
+        GameObject temp = Instantiate(placedObject, placedObject.transform.position, Quaternion.identity);
+        temp.transform.SetParent(parentObject.transform);
+       
+    }
+}
---
+++ Assets/ShowCordinates.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Android;
+
+public class ShowCordinates : MonoBehaviour
+{
+    public Text longtitudeText;
+    public Text latitudeText;
+    public Text accuracyText;
+    /*IEnumerator Start()
+    {
+        Input.location.Start();
+        int maxWait = 20;
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+        if (maxWait < 1)
+        {
+            print("Timed out");
+            yield break;
+        }
+
+        if (Input.location.status == LocationServiceStatus.Failed)
+        {
+            print("Unable to determine device location");
+            yield break;
+        }
+        else
+        {
+            // Access granted and location value could be retrieved
+            print("Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
+        }
+
+        // Stop service if there is no need to query location updates continuously
+        Input.location.Stop();
+    }*/
+    private void Start()
+    {
+        if (!Permission.HasUserAuthorizedPermission(Permission.FineLocation))
+        {
+            Permission.RequestUserPermission(Permission.FineLocation);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        longtitudeText.text = "Longtitude: " + Input.location.lastData.longitude;
+        latitudeText.text = "Latitude: " + Input.location.lastData.latitude;
+        accuracyText.text = "Accuracy: " + Input.location.lastData.horizontalAccuracy;
+    }
+}
